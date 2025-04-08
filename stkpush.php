5fa00c98b2b224258fd4ff633f1eabf4cb3f56cf
Simon-symo <simonnthiga9@gmail.com>
2025-04-08 11:22:27 +0300

    Update stkpush.php

--- stkpush.php
+++ stkpush.php
@@ -3,8 +3,8 @@
 $consumerKey = "7GwdVVKtmQ4nIf7ylTigS2GEfA1ZhxANXjgAoIB0IhNeJtRS";
 $consumerSecret = "jtlxnz7FmiTcH6fXAzwUaL0N92rSrQXiIs7T5U1sMdgxqAdO06c90pJlkaRWNY6l";
 $businessShortCode = "174379";
-$lipaNaMpesaPasskey = "Os6VCCnGofAz84i7XSLdUETa/oLB80AYh1JmeXHC6Ig8kxMS/cOHgLW+CBWZWTZ2+u+9zlRml/r+IeaxQGoaUbbPs98j/BF7C8nyxdrw4jauAkprb+Uj8grrR2bZEqP9DqL/RFQQcgoUxWjHN5kNS1nnI/O4rZ+3zgCZ0I2gbIqCeeGix5uOcBvc9AqZ7u+Jc77X6Sq+lo2PmhDJ51EVFiPiRWq2HVZGbIx3Jv32vyh7Q1AbdR2mfe+l4XJyySed//IofupIhNevUoDT8WC50QRbCSf6gLkDYtJOs5CLDR4dCsNvMbu7ePVSX5WFYBsQOE/MZP5pW0M+kYlyLQu44g==";
-$callbackURL = "https://yourwebsite.com/callback.php";
+$lipaNaMpesaPasskey = "";
+$callbackURL = "https://mydomain.com/pat";
 
 // Get user input
 $phone = $_POST['phone'];
@@ -13,7 +13,7 @@ $timestamp = date("YmdHis");
 $password = base64_encode($businessShortCode . $lipaNaMpesaPasskey . $timestamp);
 
 // Generate Access Token
-$accessTokenUrl = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+$accessTokenUrl = "AqjGUuOP7AhCtzzwNov7hjbdSuSUh66xQi/VEWGskgMypbD+KJxEoRPlx+wEql/UNKDDUB+kAVW2C1oVDMBMgKdhzHbTAXSJ9UDJunhzfCeMXFn+KMWvafekjPmr5ylUmfZi5rfXtLLU8YcwbqcrZCPTld1I1HyE6+txjYzAaWFfK1S/vmSRVPbHUMUcHkg7SgrbGcviMFtVW76UN7QmLoTy1/j87RMthugInaAO2/m5/8aejU9N/gZWItp8y9nZSfHyv0BqH73al8zffQfBFsL6cUFq5bfADpnwEBXMzTUwrvnqZ58sHZWp8+HQgL5EYZOlqeCKbifyvhmmONpcqA==";
 $credentials = base64_encode("$consumerKey:$consumerSecret");
 
 $ch = curl_init($accessTokenUrl);
